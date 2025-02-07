9c52dedb09de0f33d50d0174a280f78381219fa4
xmc811 <mxu.china@gmail.com>
2025-02-07 00:16:32 -0500

    Initial commit

---
+++ test/aws-cdk-project-atlas.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkProjectAtlas from '../lib/aws-cdk-project-atlas-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-project-atlas-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkProjectAtlas.AwsCdkProjectAtlasStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
