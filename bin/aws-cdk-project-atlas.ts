c9b686255176755ea9d4db918a5c5e7b4ede0727
xmc811 <mxu.china@gmail.com>
2025-02-07 23:05:10 -0500

    set up a minimal glue database with one table

--- bin/aws-cdk-project-atlas.ts
+++ bin/aws-cdk-project-atlas.ts
@@ -2,10 +2,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { SingleBucketStack } from '../lib/single-bucket-stack';
 import { S3SNSNotificationStack } from '../lib/s3-sns-notification-stack';
+import { S3GlueDataCatalogStack } from '../lib/s3-glue-data-catalog-stack';
 
 const app = new cdk.App();
 new SingleBucketStack(app, 'FirstBucket');
+
 new S3SNSNotificationStack(app, 'S3SNSNotification', {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+    }
+  }
+);
+
+new S3GlueDataCatalogStack(app, 'S3GlueDataCatalog', {
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
       region: process.env.CDK_DEFAULT_REGION
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
 export const FIRST_BUCKET_NAME = 'mingchu-first-bucket';
+export const GLUE_CATALOG_BUCKET_NAME = 'mingchu-glue-data';
